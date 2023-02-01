3e42cddb75305f24ed2afbf8478b79df1e6859d0
Katie McLaughlin <katie@glasnt.com>
2023-02-01 13:07:07 +1100

    Test (and run) black

--- server/store/tests.py
+++ server/store/tests.py
@@ -29,13 +29,11 @@ client = APIClient()
 factory = APIRequestFactory()
 
 
-
 class DecimalEncoder(json.JSONEncoder):
     def default(self, obj):
         if isinstance(obj, Decimal):
             return float(obj)
         return json.JSONEncoder.default(self, obj)
-
 
 
 class AvocanoUnitTest(TransactionTestCase):
