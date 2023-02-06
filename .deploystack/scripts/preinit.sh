b3b48c453671f76269ba4ae3ea37cfffc5223d0d
Katie McLaughlin <katie@glasnt.com>
2023-02-06 11:40:36 +1100

    Pre-setup APIs, cloud build permissions

--- .deploystack/scripts/preinit.sh
+++ .deploystack/scripts/preinit.sh
@@ -1,5 +1,14 @@
 # Before Terraform
 
+echo "Enable additional APIs"
+gcloud services enable cloudresourcemanager.googleapis.com cloudbuild.googleapis.com iam.googleapis.com
+
+echo "Update Cloud Build permissions"
+export CLOUDBUILD_SA="$(gcloud projects describe $PROJECT_ID \
+    --format 'value(projectNumber)')@cloudbuild.gserviceaccount.com"
+gcloud projects add-iam-policy-binding $PROJECT_ID \
+    --member serviceAccount:$CLOUDBUILD_SA --role roles/firebasehosting.admin
+
 echo "Create Firebase Image"
 gcloud builds submit --config provisioning/firebase-builder.cloudbuild.yaml --no-source
 
