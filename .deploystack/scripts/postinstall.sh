6982433a123916e8d79fc08234aeab31daa7cb35
Katie McLaughlin <katie@glasnt.com>
2023-02-06 13:00:00 +1100

    add custom success message by way of postinstall.sh

---
+++ .deploystack/scripts/postinstall.sh
@@ -0,0 +1,23 @@
+# in lieu of dynamic success messages, make the output with a script.
+
+# TODO(glasnt): this file is set by terraform, unsure how else to get user input
+REGION=$(cat provisioning/terraform/terraform.tfvars | grep region | cut -d'"' -f2)
+API_URL=$(gcloud run services describe server --format "value(status[0].url)" --region $REGION)
+ADMIN_PASSWORD=$(gcloud secrets versions access latest --secret django_admin_password)
+FIREBASE_URL="https://${GOOGLE_CLOUD_PROJECT}.web.app"
+
+
+echo "
+🥑🥑🥑🥑🥑🥑🥑🥑🥑🥑🥑🥑🥑
+
+Avocano has been deployed!
+
+Access the website: $FIREBASE_URL
+
+Log into the API:
+$API_URL
+
+Username: admin
+Password: $ADMIN_PASSWORD
+
+✨"
