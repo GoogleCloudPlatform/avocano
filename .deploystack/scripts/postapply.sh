de72e3e1845068ec140655921ef043939baf0658
Katie McLaughlin <katie@glasnt.com>
2023-02-06 13:00:11 +1100

    WORKAROUND: apply IAM after TF
    Terraform will override IAM in some cases, so set IAM we need after TF
    has processed.

--- .deploystack/scripts/postapply.sh
+++ .deploystack/scripts/postapply.sh
@@ -1,3 +1,15 @@
 # After Terraform
+
+echo "Update Cloud Build permissions"
+export CLOUDBUILD_SA="$(gcloud projects describe $GOOGLE_CLOUD_PROJECT \
+    --format 'value(projectNumber)')@cloudbuild.gserviceaccount.com"
+
+for role in firebasehosting.admin run.viewer; do
+    echo "Adding $role permissions to Cloud Build"
+    gcloud projects add-iam-policy-binding $GOOGLE_CLOUD_PROJECT \
+        --member serviceAccount:$CLOUDBUILD_SA --role roles/$role > /dev/null
+done
+
 echo "Deploy client"
 gcloud builds submit --config provisioning/client.cloudbuild.yaml
+
--- .deploystack/scripts/preinit.sh
+++ .deploystack/scripts/preinit.sh
@@ -3,15 +3,6 @@
 echo "Enable additional APIs"
 gcloud services enable cloudresourcemanager.googleapis.com cloudbuild.googleapis.com iam.googleapis.com
 
-echo "Update Cloud Build permissions"
-export CLOUDBUILD_SA="$(gcloud projects describe $GOOGLE_CLOUD_PROJECT \
-    --format 'value(projectNumber)')@cloudbuild.gserviceaccount.com"
-
-for role in firebasehosting.admin run.viewer; do
-    echo "Adding $role permissions to Cloud Build"
-    gcloud projects add-iam-policy-binding $GOOGLE_CLOUD_PROJECT \
-        --member serviceAccount:$CLOUDBUILD_SA --role roles/$role > /dev/null
-
 echo "Create Firebase Image"
 gcloud builds submit --config provisioning/firebase-builder.cloudbuild.yaml --no-source
 
