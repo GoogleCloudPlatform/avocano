41056991fa261ad6c6ff6180bec85e892777a034
Marc Dougherty <muncus@users.noreply.github.com>
2023-03-15 13:32:28 -0700

    chore(terraform): update cloud run service to v2
    This allows use of startup and liveness checks without the beta provider

--- provisioning/terraform/service.tf
+++ provisioning/terraform/service.tf
@@ -1,34 +1,39 @@
-resource "google_cloud_run_service" "server" {
+resource "google_cloud_run_v2_service" "server" {
   name                       = var.service_name
   location                   = var.region
-  autogenerate_revision_name = true
   template {
-    spec {
-      service_account_name = google_service_account.server.email
-      containers {
-        image = data.google_container_registry_image.server.image_url
-        env {
-          name = "DJANGO_ENV"
-          value_from {
-            secret_key_ref {
-              name = google_secret_manager_secret.django_settings.secret_id
-              key  = "latest"
-            }
+    service_account = google_service_account.server.email
+    containers {
+      image = data.google_container_registry_image.server.image_url
+      env {
+        name = "DJANGO_ENV"
+        value_source {
+          secret_key_ref {
+            secret = google_secret_manager_secret.django_settings.secret_id
+            version  = "latest"
           }
         }
       }
-    }
-    metadata {
-      annotations = {
-        "autoscaling.knative.dev/maxScale"      = "100"
-        "run.googleapis.com/cloudsql-instances" = google_sql_database_instance.postgres.connection_name
-        "run.googleapis.com/client-name"        = "terraform"
+      startup_probe {
+        http_get {
+          path = "/ready"
+        }
       }
+      liveness_probe {
+        http_get {
+          path = "/healthy"
+        }
+      }
+    }
+    annotations = {
+      "autoscaling.knative.dev/maxScale"      = "100"
+      "run.googleapis.com/cloudsql-instances" = google_sql_database_instance.postgres.connection_name
+      "run.googleapis.com/client-name"        = "terraform"
     }
   }
   traffic {
-    percent         = 100
-    latest_revision = true
+    type = "TRAFFIC_TARGET_ALLOCATION_TYPE_LATEST"
+    percent = 100
   }
 
   depends_on = [
@@ -46,8 +51,8 @@ data "google_iam_policy" "noauth" {
 }
 
 resource "google_cloud_run_service_iam_policy" "server_noauth" {
-  location    = google_cloud_run_service.server.location
-  project     = google_cloud_run_service.server.project
-  service     = google_cloud_run_service.server.name
+  location    = google_cloud_run_v2_service.server.location
+  project     = google_cloud_run_v2_service.server.project
+  service     = google_cloud_run_v2_service.server.name
   policy_data = data.google_iam_policy.noauth.policy_data
 }
