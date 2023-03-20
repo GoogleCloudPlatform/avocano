c805c3d61727876fec932fcf7b3961022f1521ad
Marc Dougherty <muncus@users.noreply.github.com>
2023-03-20 15:21:56 -0700

    remove post_fork, and pin to one worker
    a single worker avoids the need for post_fork hooks in gunicorn.

--- server/gunicorn_config.py
+++
@@ -1,4 +0,0 @@
-
-# This hook is needed to avoid deadlocks in the opentelemetry batch span processor.
-def post_fork(server,worker):
-    from opentelemetry.instrumentation.auto_instrumentation import sitecustomize
