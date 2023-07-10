3d301f51e540f8a336f845a05437f8c1b34de1b6
Katie McLaughlin <katie@glasnt.com>
2023-07-10 14:33:01 +1000

    fix: remove autodiscovery for otel

--- server/manage.py
+++ server/manage.py
@@ -19,10 +19,14 @@
 import os
 import sys
 
+from opentelemetry.instrumentation.django import DjangoInstrumentor
 
 def main():
     """Run administrative tasks."""
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "avocano_api.settings")
+
+    DjangoInstrumentor().instrument()
+
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
