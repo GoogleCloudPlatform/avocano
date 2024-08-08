2acc5182292f1def2912b518976616c75af98397
Katie McLaughlin <glasnt@google.com>
2024-08-08 13:34:28 +1000

    feat: update to Django 5.1 (#467)
    * pin named dependencies
    
    * Add requirement categories
    
    * fix: update to STORAGES config
    
    * makemigrations
    
    * headers
    
    * add default for local staticfiles

--- server/avocano_api/settings.py
+++ server/avocano_api/settings.py
@@ -191,10 +191,18 @@ MEDIA_URL = "/media/"
 
 # Use Cloud Storage if configured, otherwise use local storage.
 if GS_BUCKET_NAME := env("GS_BUCKET_NAME", default=None):
+    STORAGES = {
+        "default": {"BACKEND": "storages.backends.gcloud.GoogleCloudStorage"},
+        "staticfiles": {"BACKEND": "storages.backends.gcloud.GoogleCloudStorage"},
+    }
+
     GS_DEFAULT_ACL = "publicRead"
-    DEFAULT_FILE_STORAGE = "storages.backends.gcloud.GoogleCloudStorage"
-    STATICFILES_STORAGE = "storages.backends.gcloud.GoogleCloudStorage"
 else:
-    DEFAULT_FILE_STORAGE = "django.core.files.storage.FileSystemStorage"
+    STORAGES = {
+        "default": {"BACKEND": "django.core.files.storage.FileSystemStorage"},
+        "staticfiles": {
+            "BACKEND": "django.contrib.staticfiles.storage.StaticFilesStorage",
+        },
+    }
     STATIC_ROOT = os.path.join(BASE_DIR, STATIC_URL.replace("/", ""))
     MEDIA_ROOT = os.path.join(BASE_DIR, MEDIA_URL.replace("/", ""))
